24a32e1b497f00ddca14b9f242fab7d2d183d327
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-19 00:37:58 +0200

    Use base64-regex module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var base64Regex = require('base64-regex');
+
 /**
  * Get base64 from a string
  *
@@ -8,8 +10,7 @@
  */
 
 module.exports = function (str) {
-    var reg = /(^|\s)(?:[A-Za-z0-9+\/]{4}\n?)*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)(?=\s|$)/gi;
-    var base64 = str.match(reg);
+    var base64 = str.match(base64Regex());
     var ret = [];
 
     if (!base64) {
